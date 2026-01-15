d4feab22d8038e71b34280caef71806429060855
Safar Umarov <safarumarov711@gmail.com>
2026-01-16 02:19:02 +0700

    Add sumOddLengthSubarrays function to calculate the sum of all odd-length subarrays

---
+++ solutions/easy/1588/main.go
@@ -0,0 +1,34 @@
+package main
+
+func sumOddLengthSubarrays(arr []int) int {
+	arr = sumPrefix(arr)
+	s := arr[len(arr)-1]
+	for i := 2; i < len(arr); i++ {
+		if i%2 != 0 {
+			continue
+		}
+
+		l, r := 0, i
+		for r < len(arr) && (r-1)-l <= i {
+			if l == 0 {
+				s += arr[i]
+				r++
+				l++
+				continue
+			}
+			s += arr[r] - arr[l-1]
+			l++
+			r++
+		}
+	}
+
+	return s
+}
+
+func sumPrefix(arr []int) []int {
+	for i := 1; i < len(arr); i++ {
+		arr[i] = arr[i-1] + arr[i]
+	}
+
+	return arr
+}
