1559819ed5c30367e2fb1e04b56c22547ff492af
Safar Umarov <safarumarov711@gmail.com>
2026-01-21 01:46:29 +0700

    Add maximumPopulation function to determine the year with the highest population from logs

---
+++ solutions/easy/1854/maximum_population_year.go
@@ -0,0 +1,24 @@
+package maximumpopulationyear
+
+func maximumPopulation(logs [][]int) int {
+	years := make(map[int]int)
+
+	for _, row := range logs {
+		for i := row[0]; i < row[1]; i++ {
+			years[i]++
+		}
+	}
+
+	earlyYear := 0
+	mxCount := 0
+	for year, count := range years {
+		if count > mxCount {
+			earlyYear = year
+			mxCount = count
+		} else if count == mxCount && year < earlyYear {
+			earlyYear = year
+		}
+	}
+
+	return earlyYear
+}
