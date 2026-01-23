69060549c1e4affb9bf5c26f9ac89ceb9877a979
Safar Umarov <safarumarov711@gmail.com>
2026-01-24 01:38:04 +0700

    Add findMiddleIndexPrefixSumClassic function to find the middle index using prefix sums

--- solutions/easy/1991/main.go
+++ solutions/easy/1991/main.go
@@ -27,3 +27,31 @@ func findSum(nums []int) int {
 
 	return s
 }
+
+func findMiddleIndexPrefixSumClassic(nums []int) int {
+	n := len(nums)
+	ps := make([]int, n)
+
+	i := 1
+	ps[0] = nums[0]
+
+	for i < n {
+		ps[i] = nums[i] + ps[i-1]
+		i++
+	}
+
+	right := ps[n-1]
+	for i := 0; i < n; i++ {
+		var left int
+		if i == 0 {
+			left = 0
+		} else {
+			left = ps[i-1]
+		}
+		if right-left-nums[i] == left {
+			return i
+		}
+	}
+
+	return -1
+}
