61361cf825704627334012ee409878b697460450
Safar Umarov <safarumarov711@gmail.com>
2026-03-20 18:31:31 +0700

    Add countSubmatrices function to count submatrices with sum less than k

---
+++ solutions/medium/3070/main.go
@@ -0,0 +1,19 @@
+package main
+
+func countSubmatrices(grid [][]int, k int) int {
+	n, m := len(grid), len(grid[0])
+	cols := make([]int, m)
+
+	res := 0
+	for i := 0; i < n; i++ {
+		rows := 0
+		for j := 0; j < m; j++ {
+			cols[j] += grid[i][j]
+			rows += cols[j]
+			if rows < k {
+				res++
+			}
+		}
+	}
+	return res
+}
