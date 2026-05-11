267592b7b7bc976325929105e1a1a59174f5a8c8
Safar Umarov <safarumarov711@gmail.com>
2026-05-12 00:49:23 +0700

    200. Number of Islands: with 2d array

--- solutions/medium/number_of_islands/number_of_islands.go
+++ solutions/medium/number_of_islands/number_of_islands.go
@@ -4,15 +4,19 @@ package numberofislands
 // Link: https://leetcode.com/problems/number-of-islands/description/
 
 func numIslands(grid [][]byte) int {
-	visited := make(map[string]bool)
 	islands := 0
 	rows, cols := len(grid), len(grid[0])
+
+	visited := make([][]bool, rows)
+	for i := range rows {
+		visited[i] = make([]bool, cols)
+	}
 
 	var dfs func(i, j int)
 
 	dfs = func(i, j int) {
 		queue := [][2]int{{i, j}}
-		visited[key(i, j)] = true
+		visited[i][j] = true
 		directions := [][2]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
 		for len(queue) > 0 {
 			curr := queue[0]
@@ -20,23 +24,20 @@ func numIslands(grid [][]byte) int {
 
 			for _, dir := range directions {
 				in, jn := curr[0]+dir[0], curr[1]+dir[1]
-				_, ok := visited[key(in, jn)]
-
-				if ok || in < 0 || jn < 0 || jn >= cols || in >= rows || grid[in][jn] != '1' {
+				if in < 0 || jn < 0 || jn >= cols || in >= rows || visited[in][jn] || grid[in][jn] != '1' {
 					continue
 				}
 
-				visited[key(in, jn)] = true
+				visited[in][jn] = true
 
 				queue = append(queue, [2]int{in, jn})
 			}
 		}
 	}
 
-	for i := range grid {
+	for i := 0; i < len(grid); i++ {
 		for j := 0; j < len(grid[0]); j++ {
-			_, ok := visited[key(i, j)]
-			if grid[i][j] == '1' && !ok {
+			if grid[i][j] == '1' && !visited[i][j] {
 				islands++
 				dfs(i, j)
 			}
@@ -44,8 +45,4 @@ func numIslands(grid [][]byte) int {
 	}
 
 	return islands
-}
-
-func key(i, j int) string {
-	return string(i) + "," + string(j)
 }
